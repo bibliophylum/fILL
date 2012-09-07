1fee1dd23dfa4ef3fefdaade4f49a4165e613c94
David Andrew Christensen <David.A.Christensen@gmail.com>
2012-09-07 14:17:47 -0500

    contacts list email address is clickable

--- modules/fILL/info.pm
+++ modules/fILL/info.pm
@@ -2,12 +2,14 @@
 #    fILL - Free/Open-Source Interlibrary Loan management system
 #    Copyright (C) 2011  David A. Christensen
 #
-#    This program is free software: you can redistribute it and/or modify
+#    info.pm is a part of fILL.
+#
+#    fILL is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
+#    fILL is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
