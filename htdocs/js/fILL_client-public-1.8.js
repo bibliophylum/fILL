d48a37f71e427ffd6e848611e44dd4542babce60
Anthony <awcomix@gmail.com>
2014-08-19 16:10:16 -0500

    dev test, make sec red

--- htdocs/js/fILL_client-public-1.8.js
+++ htdocs/js/fILL_client-public-1.8.js
@@ -465,7 +465,7 @@ function primaryDetails(data) {
 }
 
 function secondaryDetails(data) {
-    var secondary = '<table class="secondary_info">';  // start new table for location info
+    var secondary = '<table class="secondary_info" style="color:red;">';  // start new table for location info
 
     var len=data["location"].length;
     for (var i=0; i<len; i++) {
