1550aa61d91cd4b1042185d241e5202e12ac8a28
Anthony <awcomix@gmail.com>
2014-08-20 13:25:34 -0500

    small edit

--- htdocs/js/fILL_client-public-1.8.js
+++ htdocs/js/fILL_client-public-1.8.js
@@ -557,7 +557,7 @@ function buildRequestForm(data) {
     if (isElectronicResource) {
 	requestForm += '<p><strong>This electronic resource is not requestable through ILL.</strong></p>';
     } else {
-	requestForm += '<input type="submit" value="Click to request">';
+	requestForm += '<input type="submit" value="Click to Request">';
     }
     requestForm += '</form>';
 
