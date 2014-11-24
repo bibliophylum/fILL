3cf37ce1fe4baf9d2152533328d08e5b94f39429
David Christensen <David.A.Christensen@gmail.com>
2014-11-24 11:38:37 -0600

    need medium in acq

--- updates/22.add-isbn-pubdate-to-request-group.sql
+++ updates/22.add-isbn-pubdate-to-request-group.sql
@@ -2,5 +2,6 @@
 alter table request_group add column isbn varchar(40);
 alter table request_group add column pubdate varchar(40);
 
--- and acq needs a default timestamp
+-- and acq needs a default timestamp and a 'medium' column
 alter table acquisitions alter ts set default now();
+alter table acquisitions add column medium varchar(80);
