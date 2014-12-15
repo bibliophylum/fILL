5eabf9e268f6fc3041c748193abe909dd8992dac
David Christensen <David.A.Christensen@gmail.com>
2014-12-15 10:23:59 -0600

    update to the update

--- updates/21.acquisitions-list.sql
+++ updates/21.acquisitions-list.sql
@@ -8,7 +8,9 @@ create table acquisitions (
        author varchar(256),
        isbn varchar(40),
        pubdate varchar(40),
-       ts timestamp with time zone
+       ts timestamp with time zone,
+       medium varchar(80),
+       pid integer
 );
 create index on acquisitions (lid);
 create index on acquisitions (ts);
