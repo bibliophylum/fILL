9eae690d2dfd67d6daf50b525f0f73f55f490cd7
David Christensen <David.A.Christensen@gmail.com>
2015-08-24 15:56:13 -0500

    final (hopefully) tweaks to the monthly ILL board report

--- htdocs/js/report-board-1.0.js
+++ htdocs/js/report-board-1.0.js
@@ -22,7 +22,7 @@
 $('document').ready(function(){
 
     $("#waitDiv").hide();
-    $("#content").hide();
+    $("#report-content").hide();
     $("#printButton").hide();
     $("#printNote").hide();
 
@@ -30,14 +30,14 @@ $('document').ready(function(){
     $("#month").val( new Date().getMonth() );
 
     $("#printButton").on("click", function() {
-	$("#content").printElement();
+	$("#report-content").printElement();
     });
 
     $("#dateButton").on("click", function() {
 	$("#waitDiv").show();
 	$("#printButton").hide();
 	$("#printNote").hide();
-	$("#content").hide();
+	$("#report-content").hide();
 
 	var parms = { 
 	    oid: $("#oid").text(),
@@ -61,12 +61,14 @@ $('document').ready(function(){
 		      $("#borrowing-detail-month").text( data.report.month_name );
 		      $("#borrowing-requests").text( b.requests.books_requested.total );
 		
+		      $("#borrowing-request-types").empty();
 		      $.each(b.requests.books_requested.type, function( key, value ){
 			  $("#borrowing-request-types").append("<li>"+key+": "+value+"</li>");
 		      });
 		      $("#chart-info-month").text( data.report.month_name );
 		      $("#borrowing-borrowed").text( b.we_received.total );
-		      if (b.requests_unfilled.total !== undefined) {
+		      $("#borrowing-unfilled-types").empty();
+		      if (b.requests_unfilled.total != null) {
 			  $("#borrowing-unfilled").text( b.requests_unfilled.total );
 			  $.each(b.requests_unfilled.type, function( key, value ){
 			      $("#borrowing-unfilled-types").append("<li>"+key+": "+value+"</li>");
@@ -76,10 +78,12 @@ $('document').ready(function(){
 			  $("#borrowing-unfilled-types").parent().text("We received no 'unfilled' notifications this month");
 		      } 
 
-		      if (b.change_over_previous_year !== undefined) {
+		      if (b.change_over_previous_year != null) {
 			  $("#borrowing-requests-change-base").text( b.requests.books_requested.total );
 			  $("#borrowing-requests-change-month").text( data.report.month_name );
+			  $("#borrowing-requests-change-month2").text( data.report.month_name );
 			  $("#borrowing-change").text( b.change_over_previous_year );
+			  $("#p-borrowing-change").show();
 		      } else {
 			  $("#p-borrowing-change").hide();
 		      }
@@ -89,29 +93,47 @@ $('document').ready(function(){
 		      $("#borrowing-value").text( b.value );
 
 		      // Lending
+		      $("#title2-library").text( data.report.org_name );
+		      $("#title2-month").text( data.report.month_name );
+		      $("#title2-year").text( data.report.year );
+
 		      var l = data.report.lending;
+		      $("#lending-detail-month").text( data.report.month_name );
 		      $("#lending-detail-library").text( data.report.org_name );
 		      $("#lending-requests").text( l.requests_to_lend.total );
 		      $("#lending-shipped").text( l.shipped.total );
 		      $("#lending-unfilled").text( l.responded_unfilled.total );
-		      if (l.responded_unfilled.status !== undefined) {
+		      $("#lending-unfilled-status").empty();
+		      if (l.responded_unfilled.status != null) {
 			  $.each(l.responded_unfilled.status, function( key, value ){
 			      $("#lending-unfilled-status").append("<li>"+key+": "+value+"</li>");
 			  });
 		      } else {
 			  $("#lending-unfilled-status").parent().text("We filled all ILL requests this month");
 		      }
-		      $("#lending-requests-change-base").text( l.requests_to_lend.total );
-		      $("#lending-requests-change-month").text( data.report.month_name );
-		      $("#lending-requests-change").text( l.change_over_previous_year.requests );
+		      if (l.change_over_previous_year.requests != null) {
+			  $("#lending-requests-change-base").text( l.requests_to_lend.total );
+			  $("#lending-requests-change-month").text( data.report.month_name );
+			  $("#lending-requests-change-month2").text( data.report.month_name );
+			  $("#lending-requests-change").text( l.change_over_previous_year.requests );
+			  $("#p-lending-requests-change").show();
+		      } else {
+			  $("#p-lending-requests-change").hide();
+		      }
 
-		      $("#lending-shipped-change-base").text( l.shipped.total );
-		      $("#lending-shipped-change-month").text( data.report.month_name );
-		      $("#lending-shipped-change").text( l.change_over_previous_year.shipped );
+		      if (l.change_over_previous_year.requests != null) {
+			  $("#lending-shipped-change-base").text( l.shipped.total );
+			  $("#lending-shipped-change-month").text( data.report.month_name );
+			  $("#lending-shipped-change-month2").text( data.report.month_name );
+			  $("#lending-shipped-change").text( l.change_over_previous_year.shipped );
+			  $("#p-lending-shipping-change").show();
+		      } else {
+			  $("#p-lending-shipping-change").hide();
+		      }
 		  })
 	    .success(function() {
 		//alert('success');
-		$("#content").show();
+		$("#report-content").show();
 		$("#printButton").show();
 		$("#printNote").show();
 	    })
