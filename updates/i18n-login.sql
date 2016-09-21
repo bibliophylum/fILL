a88a6bc8fdc8de9abb69707463fffb72c78759c7
David Christensen <David.A.Christensen@gmail.com>
2016-09-21 09:38:09 -0500

    almost finished login page... waiting on a few translations

--- updates/i18n-login.sql
+++ updates/i18n-login.sql
@@ -1,7 +1,5 @@
 -- i18n-login.sql
 
--- THIS IS JUST A TEST, USING GOOGLE TRANSLATE!  DO NOT USE IN PRODUCTION!
-
 insert into i18n (page,lang,category,id,stage,text) values
 ('public/login.tmpl','en','tparm','lang',null,'en'),
 ('public/login.tmpl','en','tparm','pagetitle',null,'fILL public login'),
@@ -12,19 +10,25 @@ insert into i18n (page,lang,category,id,stage,text) values
 ('public/login.tmpl','en','js_lang_data','cannot-remember',null,'If you cannot remember your user name or password, please contact your local public library.'),
 ('public/login.tmpl','en','js_lang_data','choose-region',null,'Which region of Manitoba do you live in?'),
 ('public/login.tmpl','en','js_lang_data','choose-town',null,'Where is your home library?'),
-('public/login.tmpl','en','js_lang_data','startOver',null,'Start over')
+('public/login.tmpl','en','js_lang_data','startOver',null,'Start over'),
+('public/login.tmpl','en','js_lang_data','login-legend',null,'Login'),
+('public/login.tmpl','en','js_lang_data','home-lib-text',null,'Home library location:'),
+('public/login.tmpl','en','js_lang_data','authen_loginbutton',null,'Sign in')
 ;
 
 insert into i18n (page,lang,category,id,stage,text) values
 ('public/login.tmpl','fr','tparm','lang',null,'fr'),
 ('public/login.tmpl','fr','tparm','pagetitle',null,'fILL public login'),
-('public/login.tmpl','fr','js_lang_data','image-attribution',null,'Image de '),
+('public/login.tmpl','fr','js_lang_data','image-attribution',null,'Image offerte par '),
 ('public/login.tmpl','fr','js_lang_data','menu_login',null,'Bienvenue'),
-('public/login.tmpl','fr','js_lang_data','menu_new',null,'Etes-vous de nouveau à fILL'),
-('public/login.tmpl','fr','js_lang_data','welcome-text',null,'Bienvenue au système de prêt entre bibliothèques des bibliothèques publiques du Manitoba.'),
+('public/login.tmpl','fr','js_lang_data','menu_new',null,'Vous êtes nouveau à fILL?'),
+('public/login.tmpl','fr','js_lang_data','welcome-text',null,'Bienvenue au système de prêts interbibliothèques des bibliothèques publiques du Manitoba'),
 ('public/login.tmpl','fr','js_lang_data','cannot-remember',null,'Si vous ne vous rappelez pas de votre nom d’utilisateur ou de votre mot de passe, communiquez avec votre bibliothèque publique locale.'),
 ('public/login.tmpl','fr','js_lang_data','choose-region',null,'Quelle région du Manitoba habitez-vous?'),
 ('public/login.tmpl','fr','js_lang_data','choose-town',null,'Où se trouve votre bibliothèque habituelle?'),
-('public/login.tmpl','fr','js_lang_data','startOver',null,'Recommencez')
+('public/login.tmpl','fr','js_lang_data','startOver',null,'Recommencez'),
+('public/login.tmpl','fr','js_lang_data','login-legend',null,'Connexion'),
+('public/login.tmpl','fr','js_lang_data','home-lib-text',null,'Lieu de la bibliothèque habituelle:'),
+('public/login.tmpl','fr','js_lang_data','authen_loginbutton',null,'Ouverture de session')
 ;
 
