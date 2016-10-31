e4b821977d63f52c1c7dce89a60f321157d65306
David Christensen <David.A.Christensen@gmail.com>
2016-10-31 15:37:57 -0500

    fixed typo

--- updates/i18n-about.sql
+++ updates/i18n-about.sql
@@ -6,7 +6,7 @@ insert into i18n (page,lang,category,id,stage,text) values
 ('public/about.tmpl','en','js_lang_data','image-attribution',null,'Image care of '),
 ('public/about.tmpl','en','js_lang_data','about-heading',null,'About'),
 ('public/about.tmpl','en','js_lang_data','about-p1',null,'fILL is the interlibrary loan management system for all Manitoba public libraries. It connects your local library to the millions of items available in Manitoba''s libraries.'),
-('public/about.tmpl','en','js_lang_data','about-p2',null,'If you are unable to find an item after searching your library&rsquo;s catalogue you can use fILL to find it online and have it delivered to your local public library.'),
+('public/about.tmpl','en','js_lang_data','about-p2',null,'If you are unable to find an item after searching your library''s catalogue you can use fILL to find it online and have it delivered to your local public library.'),
 ('public/about.tmpl','en','js_lang_data','about-p3',null,'fILL enables library patrons to quickly and easily search the catalogues of many Manitoba public libraries at the same time. When you find the item that you''d like to request, fILL prepares the interlibrary loan request for your public library and the local library staff to try to borrow the item from the lending library  for you.'),
 ('public/about.tmpl','en','js_lang_data','about-privacy',null,'Privacy'),
 ('public/about.tmpl','en','js_lang_data','about-privacy-p1',null,'fILL does not keep a record of your search history nor  does it keep a record of request transactions after borrowed items are returned. Logging into your account is necessary in order for your library to validate the request; some non-identifying data may be retained for statistical purposes.');
